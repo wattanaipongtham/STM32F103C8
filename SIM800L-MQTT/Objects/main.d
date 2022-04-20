c278d75ae3d90b361cee22341c623deed86bb4d8
WATTANAI PONGTHAM <m6301019@office365.sut.ac.th>
2022-04-20 16:00:13 +0700

    update

--- SIM800L-MQTT/Objects/main.d
+++ SIM800L-MQTT/Objects/main.d
@@ -1,6 +1,7 @@
 .\objects\main.o: main.c
 .\objects\main.o: H:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
 .\objects\main.o: H:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: H:\Keil_v5\ARM\ARMCC\Bin\..\include\ctype.h
 .\objects\main.o: H:\Keil_v5\Packs\Keil\STM32F1xx_DFP\2.4.0\Device\Include\stm32f10x.h
 .\objects\main.o: .\RTE\_STM32F103C8\RTE_Components.h
 .\objects\main.o: H:\Keil_v5\Packs\ARM\CMSIS\5.8.0\CMSIS\Core\Include\core_cm3.h
--- SIM800L-MQTT/main.c
+++ SIM800L-MQTT/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <ctype.h>
 #include "stm32f10x.h"
 #include "wattanai_stm32f103c8_uart1.h"
 #include "wattanai_stm32f103c8_clock_config.h"
@@ -9,7 +10,7 @@ char AT_CMD[4] 				= "AT\r\n";
 char AT_CSTT[41] 			= "AT+CSTT=\"TRUE-H INTERNET\",\"true\",\"true\"\r\n";
 char AT_CIICR[10] 		= "AT+CIICR\r\n";
 char AT_CIFSR[10] 		= "AT+CIFSR\r\n";
-char AT_CIPSTART[31] 	= "\"TCP\",\"167.71.217.190\",\"1883\"\r\n";
+char AT_CIPSTART[43] 	= "AT+CIPSTART=\"TCP\",\"167.71.217.190\",\"1883\"\r\n";
 /*=========================================================================*/
 
 
@@ -21,16 +22,15 @@ char RES_CIFSR[50];
 char RES_CIPSTART[50];
 /*=========================================================================*/
 int count=0;
-
+__IO uint32_t tmpreg;
 int main()
 {
 	system_clock_config();
-	RCC->APB2ENR	|=	(1U<<4);
-	GPIOC->CRH		|=	(0x44744444UL);
-	GPIOC->ODR		&=	~(0x2000UL);
 	uart1_rx_Init();
+	uart1_tx_Init();
 	NVIC_EnableIRQ(USART1_IRQn);
-	DMA1_channel5_init((uint32_t) RES_CMD, (uint32_t) &USART1->DR, 4);
+	DMA1_channel5_init((uint32_t) RES_CMD, (uint32_t) &USART1->DR, 50);
+	DMA1_channel4_init((uint32_t) AT_CMD, (uint32_t) &USART1->DR, 4);
 	while(1)
 	{
 	
@@ -39,23 +39,93 @@ int main()
 
 void USART1_IRQHandler(void)
 {
-	if(GPIOC->ODR == (0x0000UL))
-	{
-		__IO uint32_t tmpreg;
-		tmpreg = USART1->SR;
-		(void) tmpreg;
-		tmpreg = USART1->DR;
-		(void) tmpreg;
-		GPIOC->ODR |=	(0x2000UL);
-		count = count + 1;
-	}else
-	{
-		__IO uint32_t tmpreg;
-		tmpreg = USART1->SR;
-		(void) tmpreg;
-		tmpreg = USART1->DR;
-		(void) tmpreg;
-		GPIOC->ODR	&=	(0x0000UL);
-		count = count + 2;
+	if(USART1->SR & (1U<<4) && USART1->CR1 & (1U<<4)){
+		switch(count){
+			case 0:
+				tmpreg = USART1->SR;
+				(void) tmpreg;
+				tmpreg = USART1->DR;
+				(void) tmpreg;
+				if(RES_CMD[0] == 'O' && RES_CMD[1] == 'K')
+				{
+					count = count + 1;
+					DMA1_channel5_init((uint32_t) RES_CSTT, (uint32_t) &USART1->DR, 50);
+					DMA1_channel4_init((uint32_t) AT_CSTT, (uint32_t) &USART1->DR, 41);
+				}else
+				{
+					DMA1_channel5_init((uint32_t) RES_CMD, (uint32_t) &USART1->DR, 50);
+					DMA1_channel4_init((uint32_t) AT_CMD, (uint32_t) &USART1->DR, 4);
+				}
+				break;
+			case 1:
+				tmpreg = USART1->SR;
+				(void) tmpreg;
+				tmpreg = USART1->DR;
+				(void) tmpreg;
+				if(RES_CSTT[0] == 'O' && RES_CSTT[1] == 'K')
+				{
+					count = count + 1;
+					DMA1_channel5_init((uint32_t) RES_CIICR, (uint32_t) &USART1->DR, 50);
+					DMA1_channel4_init((uint32_t) AT_CIICR, (uint32_t) &USART1->DR, 10);
+				}else
+				{
+					DMA1_channel5_init((uint32_t) RES_CSTT, (uint32_t) &USART1->DR, 50);
+					DMA1_channel4_init((uint32_t) AT_CSTT, (uint32_t) &USART1->DR, 41);
+				}
+				break;
+			case 2:
+				tmpreg = USART1->SR;
+				(void) tmpreg;
+				tmpreg = USART1->DR;
+				(void) tmpreg;
+				if(RES_CIICR[0] == 'O' && RES_CIICR[1] == 'K')
+				{
+					count = count + 1;
+					DMA1_channel5_init((uint32_t) RES_CIFSR, (uint32_t) &USART1->DR, 50);
+					DMA1_channel4_init((uint32_t) AT_CIFSR, (uint32_t) &USART1->DR, 10);
+				}else
+				{
+					DMA1_channel5_init((uint32_t) RES_CIICR, (uint32_t) &USART1->DR, 50);
+					DMA1_channel4_init((uint32_t) AT_CIICR, (uint32_t) &USART1->DR, 10);
+				}
+				break;
+			case 3:
+				tmpreg = USART1->SR;
+				(void) tmpreg;
+				tmpreg = USART1->DR;
+				(void) tmpreg;
+				if(isdigit(RES_CIFSR[0]) && isdigit(RES_CIFSR[1]))
+				{
+					count = count + 1;
+					DMA1_channel5_init((uint32_t) RES_CIPSTART, (uint32_t) &USART1->DR, 50);
+					DMA1_channel4_init((uint32_t) AT_CIPSTART, (uint32_t) &USART1->DR, 43);
+				}else
+				{
+					DMA1_channel5_init((uint32_t) RES_CIFSR, (uint32_t) &USART1->DR, 50);
+					DMA1_channel4_init((uint32_t) AT_CIFSR, (uint32_t) &USART1->DR, 10);
+				}
+				break;
+			case 4:
+				tmpreg = USART1->SR;
+				(void) tmpreg;
+				tmpreg = USART1->DR;
+				(void) tmpreg;
+				count = count + 1;
+				break;
+			case 5:
+				tmpreg = USART1->SR;
+				(void) tmpreg;
+				tmpreg = USART1->DR;
+				(void) tmpreg;
+				count = count + 1;
+				break;
+			default :
+				tmpreg = USART1->SR;
+				(void) tmpreg;
+				tmpreg = USART1->DR;
+				(void) tmpreg;
+				count = count + 1;
+				break;
+		}
 	}
 }
